89d4dd3b1bf3363cc299bf2eec2444ca11fed9ee
hashinidesilva <hashmdesilva@gmail.com>
2023-09-06 01:29:25 +0530

    Added test data for category and product tables

--- product-service/src/main/resources/db/migration/V1__create_category_table.sql
+++ product-service/src/main/resources/db/migration/V1__create_category_table.sql
@@ -4,3 +4,18 @@ CREATE TABLE category
     name text NOT NULL
 );
 
+INSERT INTO category(name)
+VALUES ('Baby'),
+       ('Beauty & Personal Care'),
+       ('Books'),
+       ('Boys'' Fashion'),
+       ('Computers'),
+       ('Electronics'),
+       ('Girls'' Fashion'),
+       ('Health & Household'),
+       ('Home & Kitchen'),
+       ('Men''s Fashion'),
+       ('Pet Supplies'),
+       ('Sports'),
+       ('Toys & Games'),
+       ('Women''s Fashion');
