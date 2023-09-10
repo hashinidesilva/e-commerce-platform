c86f814a237545b23e0d06745cbcbe849c904316
hashinidesilva <hashmdesilva@gmail.com>
2023-09-10 21:54:31 +0530

    Added cart header

--- e-commerce-web/src/components/pages/CartPage.jsx
+++ e-commerce-web/src/components/pages/CartPage.jsx
@@ -1,13 +1,27 @@
-import { Card, Divider, Grid, IconButton, Stack, Typography } from "@mui/material";
+import { Card, CardContent, Checkbox, Divider, Grid, IconButton, Stack, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 import DeleteIcon from '@mui/icons-material/Delete';
 import InventoryIcon from "@mui/icons-material/Inventory";
 import { QuantityCounter } from "../common/QuantityCounter.jsx";
 import { OrderSummary } from "../order/OrderSummary.jsx";
 
+const CartHeader = () => {
+  return (
+    <>
+      <Typography component="div" variant="h5" fontWeight="bold">
+        Shopping Cart
+      </Typography>
+      <Typography component="div" variant="body2">
+        No items selected. {'Select all items'}
+      </Typography>
+    </>
+  );
+};
+
 const CartItem = ({item}) => {
   return (
     <Stack direction={"row"} spacing={4} sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+      <Checkbox/>
       <InventoryIcon sx={{fontSize: 80}}/>
       <Typography component="div" variant="subtitle1" sx={{flex: 2}}>
         Apple AirPods Pro (2nd Generation) Wireless Earbuds, Up to 2X More Active Noise Cancelling, Adaptive
@@ -35,8 +49,10 @@ export const CartPage = () => {
     <Grid container spacing={2}
           sx={{marginTop: "50px", display: 'flex', justifyContent: "center"}}>
       <Grid item xs={9}>
-        <Card sx={{display: 'flex', padding: 2, alignItems: 'center'}}>
-          <Stack>
+        <Card sx={{padding: 2, alignItems: 'center'}}>
+          <CartHeader is/>
+          <Divider/>
+          <Stack sx={{mt: 3}}>
             {ITEMS.map((item) => (
               <>
                 <CartItem item={item}/>
@@ -44,6 +60,11 @@ export const CartPage = () => {
               </>
             ))}
           </Stack>
+          <CardContent style={{textAlign: 'right'}}>
+            <Typography component="div" variant="body1" fontWeight="bold">
+              Subtotal (1 item): $129.97
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
       <Grid item xs={3}>
