0e899fe56e31b23c81dfe910a18c2114ebc7718a
hashinidesilva <hashmdesilva@gmail.com>
2023-11-21 20:26:15 +0530

    Renamed queue

--- rating-service/src/main/scala/com/hashini/services/rating/rabbitmq/ProductClient.scala
+++ rating-service/src/main/scala/com/hashini/services/rating/rabbitmq/ProductClient.scala
@@ -1,30 +1,30 @@
 package com.hashini.services.rating.rabbitmq
 
 import com.rabbitmq.client.AMQP.{Exchange, Queue}
-import com.rabbitmq.client.{BuiltinExchangeType, Channel}
+import com.rabbitmq.client.BuiltinExchangeType
 import com.typesafe.scalalogging.LazyLogging
 
 class ProductClient(name: String) extends LazyLogging {
 
-  private val queueName: String = name
+  private val queueName: String = name + "-rating"
   private val exchangeName: String = queueName + "-exchange"
   private val connection = RabbitConnection.connection
   private val channel = connection.createChannel()
-  createExchange(channel)
-  createQueue(channel)
-  channel.queueBind(queueName, exchangeName, "")
+  createExchange()
+  createQueue()
+  channel.queueBind(queueName, exchangeName, "rating")
   channel.confirmSelect()
 
   def sendMessage(message: String): Unit = {
-    channel.basicPublish(exchangeName, "", null, message.getBytes("UTF-8"))
+    channel.basicPublish(exchangeName, "rating", null, message.getBytes("UTF-8"))
     logger.info("Message sent: " + message)
   }
 
-  private def createExchange(channel: Channel): Exchange.DeclareOk = {
+  private def createExchange(): Exchange.DeclareOk = {
     channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT, true, false, null)
   }
 
-  private def createQueue(channel: Channel): Queue.DeclareOk = {
+  private def createQueue(): Queue.DeclareOk = {
     channel.queueDeclare(queueName, true, false, false, null)
   }
 }
